d7f324ccefb8f87b6f63afd04097dedd10f44566
Ivan Shvedunov <ivan4th@gmail.com>
2020-05-13 15:44:34 +0300

    Improve docker builds
    Build separate build image that is also used to run the internal
    tests (that replaces Dockerfile.test). The image is tagged with a hash
    that changes when one of these files or subtrees change:
      Makefile build/external extras/docker/Dockerfile.build
    Reorganize Dockerfiles to make better use of layer caching
    Add directories used for local building to .dockerignore

---
+++ extras/docker/ensure-build-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# not compatible with alpine's sh
+# set -o errtrace
+# cd "$(dirname "${BASH_SOURCE}")/../.."
+
+: "${BUILD_IMAGE_NAME:=quay.io/travelping/upf-build}"
+: "${PUSH_BUILD_IMAGE:=}"
+
+build_hash="$(git ls-tree HEAD -- Makefile build/external extras/docker/Dockerfile.build | md5sum | awk '{print $1}')"
+build_image="${BUILD_IMAGE_NAME}:${build_hash}"
+
+if [[ ! $(docker images -q "${build_image}") ]] && ! docker pull "${build_image}"; then
+  DOCKER_BUILDKIT=1 docker build -f extras/docker/Dockerfile.build -t "${build_image}" .
+  if [[ ${PUSH_BUILD_IMAGE} ]]; then
+    docker push "${build_image}"
+  fi
+fi
+
+docker tag "${build_image}" "${BUILD_IMAGE_NAME}"
+if [[ ${PUSH_BUILD_IMAGE} ]]; then
+  docker push "${BUILD_IMAGE_NAME}"
+fi
